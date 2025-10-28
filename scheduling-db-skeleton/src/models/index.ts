63ff09877c44eef7fe41fca58f57f1786df7934e
YTN1112 <tonynguyenrahs@gmail.com>
2025-10-27 20:47:01 -0700

    reverted add

--- scheduling-db-skeleton/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export { User, type IUser, type UserDoc } from "./user.model";
-export { Event, type IEvent, type EventDoc } from "./event.model";
-export { EventTime, type IEventTime, type EventTimeDoc } from "./eventTime.model";
-export { Availability, type IAvailability, type AvailabilityDoc } from "./availability.model";
--- scheduling-db-skeleton/src/models/types.ts
+++
@@ -1,4 +0,0 @@
-export type EventStatus = "draft" | "open" | "closed" | "archived";   // UML: Status
-export type AvailabilityResponse = "yes" | "no" | "maybe";            // UML: AvailabilityEnum
-export type TimeFormat = "12h" | "24h";                               // UML: Timeformat
-export type TimeZoneIANA = string;                                    // UML: TimeZone (store IANA)
