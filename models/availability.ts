38223869975d354cb2d1b5ae210ed347cb7d07e7
YTN1112 <tonynguyenrahs@gmail.com>
2025-10-30 00:51:29 -0700

    Modify availability schema to use time field
    Replaced startAt and endAt fields with a time field.

--- models/availability.ts
+++ models/availability.ts
@@ -5,8 +5,7 @@ const availabilitySchema = new mongoose.Schema(
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
         status: { type: String, enum: ["available", "unavailable", "maybe"], default: "available" },
-        startAt: { type: Date, required: true },
-        endAt: { type: Date, required: true }
+        time: { type: dateTime}
     }
 );
 
