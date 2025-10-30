05947c6966a7073be132d46bfdb90d999831b128
1HongyeXiong <hongye.xiong@outlook.com>
2025-10-30 13:35:47 -0700

    Add Bootstrap frontend to display events and connect with Express API

--- server.ts
+++ server.ts
@@ -12,11 +12,12 @@ dotenv.config();
 const app = express();
 
 
-
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static("frontend"));
+
 
 // Database connection
 connectDB();
