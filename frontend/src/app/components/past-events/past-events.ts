d3b1af99a8d3baff74ee48fe6c0943c3869770b1
1HongyeXiong <hongye.xiong@outlook.com>
2025-11-13 12:30:37 -0800

    Add PastEventsComponent for displaying past events in the components folder

---
+++ frontend/src/app/components/past-events/past-events.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-past-events',
+  imports: [],
+  templateUrl: './past-events.html',
+  styleUrl: './past-events.css',
+})
+export class PastEvents {
+
+}
