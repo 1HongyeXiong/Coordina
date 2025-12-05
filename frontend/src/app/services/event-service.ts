7621528a77c0df47ca5699150111ba853499b03c
1HongyeXiong <hongye.xiong@outlook.com>
2025-12-05 13:43:15 -0800

    change event service ts

--- frontend/src/app/services/event-service.ts
+++ frontend/src/app/services/event-service.ts
@@ -2,24 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Event } from '../models/event';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-  private apiUrl = 'http://localhost:5001/api/events'; // backend base URL
+
+  private apiUrl = `${environment.apiConfig.baseUrl}/events`;
 
   constructor(private http: HttpClient) {}
 
-  // Get all events (optionally filtered by userId)
   getEvents(userId?: string): Observable<Event[]> {
-    console.log('Fetching events from', this.apiUrl);
-    
     let params = new HttpParams();
     if (userId) {
       params = params.set('userId', userId);
     }
-    
     return this.http.get<Event[]>(this.apiUrl, { params });
   }
 
